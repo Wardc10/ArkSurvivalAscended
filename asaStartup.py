58870bced5b8c2fd7a157e943b38c0ba247694f8
Christopher Ward <wardc10@go.stockton.edu>
2024-09-13 18:20:57 -0400

    added content
    added script plus documentation

---
+++ asaStartup.py
@@ -0,0 +1,24 @@
+import pyautogui
+
+while True:
+    #wait for input to begin script, ensures user is ready with ASA open before starting
+    input("-=-Press any key to run script-=-") 
+
+    #list of commands to run in ASA console
+    commands = ['r.VolumetricCloud 0', 'r.VolumetricFog 0', 'r.Water.SingleLayer.Reflection 0', 'r.DistanceFieldShadowing 1']
+
+    #loop through commands list, entering specified commands/values into in-game console one by one
+    for i in commands:
+        #open in-game console
+        pyautogui.press('`')
+        #use pyautogui to type current command into in-game console, interval of 0.05 sec between keystrokes
+        pyautogui.write(str(i), interval=0.05)
+        #enter command
+        pyautogui.press('enter')
+        #command completion msg
+        print(str(i), "Entered..")
+
+    #Exit msg
+    print("All Commands Entered Successfully!")
+    print("Enjoy Your Time in the ARK!")
+    print('\n')
